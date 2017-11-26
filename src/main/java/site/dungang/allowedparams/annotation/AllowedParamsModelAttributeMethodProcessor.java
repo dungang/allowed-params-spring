027f41bbf561d61581ca25312c574cb9e3b32391
dungang <dungang@126.com>
2017-11-26 21:36:14 +0800

    fix cast exception

--- src/main/java/site/dungang/allowedparams/annotation/AllowedParamsModelAttributeMethodProcessor.java
+++ src/main/java/site/dungang/allowedparams/annotation/AllowedParamsModelAttributeMethodProcessor.java
@@ -1,24 +1,34 @@
 package site.dungang.allowedparams.annotation;
 
 import java.lang.annotation.Annotation;
+import java.util.Collections;
 import java.util.Map;
+
+import javax.servlet.ServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindException;
+import org.springframework.validation.DataBinder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.bind.support.WebRequestDataBinder;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.method.annotation.ModelFactory;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.HandlerMapping;
 
 import site.dungang.allowedparams.AllowedParamNativeWebRequest;
 
@@ -43,6 +53,7 @@ public class AllowedParamsModelAttributeMethodProcessor implements HandlerMethod
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
+		logger.debug("检查方法参数是否支持： AllowedParams 注解");
 		return parameter.hasParameterAnnotation(AllowedParams.class);
 	}
 
@@ -97,7 +108,14 @@ public class AllowedParamsModelAttributeMethodProcessor implements HandlerMethod
 	 */
 	protected Object createAttribute(String attributeName, MethodParameter methodParam,
 			WebDataBinderFactory binderFactory, NativeWebRequest request) throws Exception {
-
+		String value = getRequestValueForAttribute(attributeName, request);
+		if (value != null) {
+			Object attribute = createAttributeFromRequestValue(
+					value, attributeName, methodParam, binderFactory, request);
+			if (attribute != null) {
+				return attribute;
+			}
+		}
 		return BeanUtils.instantiateClass(methodParam.getParameterType());
 	}
 
@@ -107,13 +125,12 @@ public class AllowedParamsModelAttributeMethodProcessor implements HandlerMethod
 	 * @param request the current request
 	 */
 	protected void bindRequestParameters(WebDataBinder binder, NativeWebRequest request, String[] allowedParams) {
-		if (null == allowedParams) {
-			((WebRequestDataBinder) binder).bind(request);
-		} else {
-			NativeWebRequest allowedParamsRequest = 
-					new AllowedParamNativeWebRequest(request, allowedParams);
-			((WebRequestDataBinder) binder).bind(allowedParamsRequest);
+		ServletRequestDataBinder servletBinder = (ServletRequestDataBinder) binder;
+		if (null != allowedParams) {
+			request = new AllowedParamNativeWebRequest(request, allowedParams);
 		}
+		ServletRequest servletRequest = request.getNativeRequest(ServletRequest.class);
+		servletBinder.bind(servletRequest);
 	}
 
 	/**
@@ -150,4 +167,62 @@ public class AllowedParamsModelAttributeMethodProcessor implements HandlerMethod
 		return !hasBindingResult;
 	}
 	
+	/**
+	 * Obtain a value from the request that may be used to instantiate the
+	 * model attribute through type conversion from String to the target type.
+	 * <p>The default implementation looks for the attribute name to match
+	 * a URI variable first and then a request parameter.
+	 * @param attributeName the model attribute name
+	 * @param request the current request
+	 * @return the request value to try to convert, or {@code null} if none
+	 */
+	protected String getRequestValueForAttribute(String attributeName, NativeWebRequest request) {
+		Map<String, String> variables = getUriTemplateVariables(request);
+		String variableValue = variables.get(attributeName);
+		if (StringUtils.hasText(variableValue)) {
+			return variableValue;
+		}
+		String parameterValue = request.getParameter(attributeName);
+		if (StringUtils.hasText(parameterValue)) {
+			return parameterValue;
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected final Map<String, String> getUriTemplateVariables(NativeWebRequest request) {
+		Map<String, String> variables = (Map<String, String>) request.getAttribute(
+				HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST);
+		return (variables != null ? variables : Collections.<String, String>emptyMap());
+	}
+
+	/**
+	 * Create a model attribute from a String request value (e.g. URI template
+	 * variable, request parameter) using type conversion.
+	 * <p>The default implementation converts only if there a registered
+	 * {@link Converter} that can perform the conversion.
+	 * @param sourceValue the source value to create the model attribute from
+	 * @param attributeName the name of the attribute (never {@code null})
+	 * @param methodParam the method parameter
+	 * @param binderFactory for creating WebDataBinder instance
+	 * @param request the current request
+	 * @return the created model attribute, or {@code null} if no suitable
+	 * conversion found
+	 * @throws Exception
+	 */
+	protected Object createAttributeFromRequestValue(String sourceValue, String attributeName,
+			MethodParameter methodParam, WebDataBinderFactory binderFactory, NativeWebRequest request)
+			throws Exception {
+
+		DataBinder binder = binderFactory.createBinder(request, null, attributeName);
+		ConversionService conversionService = binder.getConversionService();
+		if (conversionService != null) {
+			TypeDescriptor source = TypeDescriptor.valueOf(String.class);
+			TypeDescriptor target = new TypeDescriptor(methodParam);
+			if (conversionService.canConvert(source, target)) {
+				return binder.convertIfNecessary(sourceValue, methodParam.getParameterType(), methodParam);
+			}
+		}
+		return null;
+	}
 }
